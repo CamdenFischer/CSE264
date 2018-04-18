e1b26fc40e0ad2e20bd0af386c0e4939dffa53c9
Camden Fischer <CamdenFischer@gmail.com>
2018-04-18 13:19:50 -0400

    Project functionality finished, styling is crap

--- Assignments/WordSearch-cwf218/wordsearchgame/public/js/scripts.js
+++ Assignments/WordSearch-cwf218/wordsearchgame/public/js/scripts.js
@@ -24,7 +24,7 @@ function login(loginname) {
             dataType: "json",
             success: function(ret) {
                 myid = ret.id;
-                console.log("Login AJAX complete. ID: " + myid);
+                console.log("Login complete. ID: " + myid);
                 getPuzzle();
             },
             error: function (jqXHR, textStatus, errorThrown) {
@@ -66,7 +66,7 @@ function submitWord(){
     
     // Set up letters array to submit each row and column
     for (var i = 0; i < cells.length; i++) {
-        letters[i] = {r: cells[i].itemRow, c: cells[i].itemCol}; 
+        letters[i] = {r: cells[i].itemRow, c: cells[i].itemCol};
     }
     $.ajax(
         "/wordsearch/submit",
@@ -80,11 +80,11 @@ function submitWord(){
             dataType: "json",
             success: function(ret) {
                 if (ret.success){
-                    console.log("AJAX complete - word found & submitted successfully");
+                    console.log("Word found & submitted successfully");
                     wordFound();
                     cells = []; //Reset cells after this is processed
                 } else {
-                    window.alert("Not a word");
+                    console.log("Not a word");
                 }
             },
             error: function (jqXHR, textStatus, errorThrown) {
@@ -97,6 +97,7 @@ function submitWord(){
     
 }
 
+// This class changes found words to a neww color
 function wordFound(){
     for (var i = 0; i < cells.length; i++){
         var td = cells[i].tdItem;
@@ -117,7 +118,7 @@ function generatePuzzle(result){
             x++;
         }
         appendString += "</tr>";
-        $('table').append(appendString);
+        $('#puzzle').append(appendString);
     }
 }
 
@@ -137,41 +138,44 @@ function attachEventHandlers() {
     });
 
     //function called when letter is clicked
-    $('table').on('click', 'td', function() {
+    $('#puzzle').on('click', 'td', function() {
         // Get row and column of the clicked cell
         var col = $(this).parent().children().index($(this));
         var row = $(this).parent().parent().children().index($(this).parent());
 
         // If the clicked cell has not been clicked yet, change class
         if ($(this).is('.notSelected')) {
-            // Change class of the sell
+            // Change class of the cell
             $(this).addClass('selected').removeClass('notSelected');
 
             // Add cell to list of currently selected cells
             var cellToPush = {itemCol: col, itemRow: row, tdItem: $(this)};
             cells.push(cellToPush);
+            //console.log($(this).html() + " clicked.");
         }
         // If it has been clicked change it back
         else if ($(this).is('.selected')){
             // Change class of the cell
             $(this).addClass('notSelected').removeClass('selected');
 
-            var index = -1;
-
-            // Get index of item to be deleted
-            for (var i = 0; i < cells.length; i++){
-
-                if (cells[i].itemCol == col && cells[i].itemRow == row){
-                    index = i;
-                }
-            }
-
-            // Remove the item
-            if (index > -1){
-                cells.splice(index, 1);
-            }
+            removeFromCells(row, col);
         }
     });     
+}
+
+// Remove a cell from the global array
+function removeFromCells(row, col){
+    var index = -1;
+
+    for (var i = 0; i < cells.length; i++){
+        if (cells[i].itemCol == col && cells[i].itemRow == row){
+            index = i;
+        }
+    }
+    // Remove the item
+    if (index > -1){
+        cells.splice(index, 1);
+    }
 }
 
 // Run the setup code when the page finishes loading
@@ -182,18 +186,55 @@ $( () => {
 
     // Returns {name, score, winner}
     socket.on('players', function(ret) {
-        players = [];
-        ret.forEach((name, score, winner) => {
-            var name = ret.name;
-            var score = ret.score;
-            var winner = ret.winner;
-            players.push({name: name, score: score, winner: winner});
-        });
-        
+        console.log("Leaderboard updated");
+        appendPlayers(ret);
     });
 
     // Returns claimed words & letters
-    socket.on('gridupdates', function() {
-
+    socket.on('gridupdates', function(ret) {
+        updateGrid(ret.words);
     });
 });
+
+function updateGrid(words){
+
+    for (var i = 0; i < words.length; i++){ // For each word
+        var word = words[i].letters;
+        console.log("Grid update: " + words[i].text);
+        for (var t = 0; t < word.length; t++){ // For each letter in the word
+            var letter = word[t];
+            
+            var row = letter.r;
+            var col = letter.c;
+
+            //console.log("row: " + row + " col: " + col);
+
+            var cellToChange = $('#puzzle tr').eq(row).find('td').eq(col);
+            removeFromCells(row, col);
+            $(cellToChange).removeClass('notSelected').removeClass('selected').addClass('locked'); 
+        }
+    }
+}
+
+// This function builds the leaderboard table
+function appendPlayers(ret){
+    // First, delete all players on the list
+    $("#leaderboard tr").empty();
+    $("#leaderboard").append("<tr><th>Player</th><th>Points</th></tr>");
+
+    // Next, add the new list of players to the list
+    for (var i = 0; i < ret.length; i++){
+        var player = ret[i];
+        var str;
+        if (player.winner){
+            str = "<tr class='winner'>";
+        } else {
+            str = "<tr class='notWinner'>"
+        }
+        str += "<td>" + player.name + "</td>";
+        str += "<td>" + player.score + "</td>";
+        str += "</tr>";
+        $("#leaderboard").append(str);
+    }
+
+}
